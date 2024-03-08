83714ac3b381b4227245732376ece525ffdc7c86
aalaa <alaamglal@gmail.com>
2024-03-07 17:53:03 -0800

    done

---
+++ README.md
@@ -0,0 +1,14 @@
+//Airbnb is a website where people can book a temporary apartment//
+
+Description:
+*First step: We can build Airbnb web Application in Python using concept of Object and Classes.
+*Second step: As We Know EVERYTHING in Python is Object, so everything in this project will be
+	      be represented as an instances of a certain class.
+*Third step: Build a Storage Sytem(json File) where we can make our database so eveyone:
+             1- Will be able to see quickly what a model should contain
+	     2- Provide default value of any attribute
+
+Description of the command interpreter:
+*How to Start it: Using Cmd Model in Our in BaseModel
+*How to Use it: Using it as command line interface where we can interact with our objects by enetring
+		inputs and receiving outputs
---
+++ models/base_model.py
@@ -0,0 +1,45 @@
+#!/usr/bin/python3
+"""Basic Module for AirBnB Project"""
+import uuid
+from datetime import datetime
+from datetime import timedelta
+
+
+class BaseModel:
+    """defines all common attributes/methods for other classes"""
+
+    def __init__(self, *args, **kwargs):
+        """initialization"""
+        if not kwargs:
+            self.id = str(uuid.uuid4())
+            self.created_at = datetime.now()
+            self.updated_at = datetime.now()
+        else:
+            d = {}
+            for key, value in kwargs.items():
+                if key != "__class__":
+                    if key == "created_at" or key == "updated_at":
+                        setattr(self, key, datetime.strptime(value, "%Y-%m-%dT%H:%M:%S.%f"))
+                    else:
+                        setattr(self, key, value)
+
+    def __str__(self):
+        dic_str = {
+                "id": self.id,
+                "created_at": self.created_at,
+                "updated_at": self.updated_at,
+                'my_number': 89,
+                'name': 'My First Model'
+                }
+        name_class = self.__class__.__name__
+        return ("[{}] ({}) {}".format(name_class, self.id, dic_str))
+
+    def save(self):
+        self.updated_at = datetime.now()
+
+    def to_dict(self):
+        dict_class = self.__dict__.copy()
+        dict_class["created_at"] = str(self.created_at.isoformat())
+        dict_class["updated_at"] = str(self.updated_at.isoformat())
+        dict_class["__class__"] = self.__class__.__name__
+        return (dict_class)
