a7185211036d66cc8df8e6cfca4e9760414df4ec
aalaa <alaamglal@gmail.com>
2024-03-10 09:28:17 -0700

    done

--- models/base_model.py
+++ models/base_model.py
@@ -14,7 +14,6 @@ class BaseModel:
             self.id = str(uuid.uuid4())
             self.created_at = datetime.now()
             self.updated_at = datetime.now()
-            models.storage.new(self)
         else:
             d = {}
             form = "%Y-%m-%dT%H:%M:%S.%f"
@@ -40,7 +39,6 @@ class BaseModel:
     def save(self):
         """update updated_time"""
         self.updated_at = datetime.now()
-        models.storage.save()
 
     def to_dict(self):
         """return dictionary of all instances"""
